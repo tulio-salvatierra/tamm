a68e9b34441e920464c2dd43c1cabb1563bc94b4
tuliosalvatierra <salvacorp@gmail.com>
2024-08-12 23:22:22 -0500

    chore: Update landing page with improved design and content

--- tamm/src/App.tsx
+++ tamm/src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import Header from "./components/ui/Header";
-import Hero from "./assets/tamm-hero.png";
 
 function App() {
   return (
@@ -9,11 +8,11 @@ function App() {
         <Header />
         <section className="bg-mainBlue text-white h-50 bg-[url('./assets/yellow.png')] bg-cover bg-no-repeat h-screen">
           <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-1 gap-4">
-            <div className="place-content-center text-left justify-evenly p-8">
-              <h1 className="text-8xl font-bold mb-6 text-lightBlue -space-x-3.5">
+            <div className="place-content-center text-left justify-evenly p-9">
+              <h1 className="text-[9rem] font-bold mb-6 text-lightBlue -space-x-3.5 tracking-tighter leading-none">
                 Keep your home sparkling clean{" "}
               </h1>
-              <p className="text-2xl w-75 pt-4">
+              <p className="text-2xl w-75 pt-4 text-lightGreen font-bold">
                 With TAMM cleaning services! We are a team of professionals who
                 are passionate about helping you achieve your goals.
               </p>
