3f87c8f73d6956bb57a8872456e7035ab7c3ebe4
tuliosalvatierra <salvacorp@gmail.com>
2024-08-12 23:33:06 -0500

    chore: Update landing page with lazy loading for images

--- tamm/src/App.tsx
+++ tamm/src/App.tsx
@@ -9,16 +9,17 @@ function App() {
         <section className="bg-mainBlue text-white h-50 bg-[url('./assets/yellow.png')] bg-cover bg-no-repeat h-screen">
           <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-1 gap-4">
             <div className="place-content-center text-left justify-evenly p-9">
-              <h1 className="text-[9rem] font-bold mb-6 text-lightBlue -space-x-3.5 tracking-tighter leading-none">
+              <h1 className="text-[9rem] font-bold mb-6 text-lightBlue tracking-tighter leading-none">
                 Keep your home sparkling clean{" "}
               </h1>
-              <p className="text-4xl w-75 pt-4 text-lightGreen ">
+              <p className="text-4xl w-75 pt-4 text-white  bg-lightGreen p-8 rounded-2xl ">
                 With TAMM cleaning services! We are a team of professionals who
                 are passionate about helping you achieve your goals.
+                <br />
+                <button className="bg-mainBlue text-white text-2xl p-3 rounded-lg mt-4 w-full hover:bg-lightBlue  ">
+                  Book now!
+                </button>
               </p>
-              <button className="bg-mainBlue text-white text-1xl p-3 rounded-lg mt-4">
-                Book now!
-              </button>
             </div>
           </div>
         </section>
