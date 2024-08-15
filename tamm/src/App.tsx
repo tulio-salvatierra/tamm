883935d03b9043666a80d43b1c2aa8602a3c4d46
Tulio Salvatierra <salvacorp@gmail.com>
2024-08-14 19:11:53 -0500

    chore: Update landing page with improved design and content

--- tamm/src/App.tsx
+++ tamm/src/App.tsx
@@ -6,15 +6,16 @@ function App() {
     <>
       <div className="App">
         <Header />
-        <section className="bg-mainBlue text-white h-50 bg-[url('./assets/yellow.png')] bg-cover bg-no-repeat h-screen">
+        <section className="bg-mainBlue text-white h-50  bg-cover bg-no-repeat h-screen place-content-center">
           <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-1 gap-4">
-            <div className="place-content-center text-left justify-evenly p-9">
-              <h1 className="text-[9rem] font-bold mb-6 text-lightBlue tracking-tighter leading-none">
+            <div className="place-content-center text-left justify-evenly md:p-9">
+              <h1 className="text-[6rem] md:text-[11rem] font-bold mb-6 text-lightGreen tracking-tighter leading-none">
                 Keep your home sparkling clean{" "}
               </h1>
-              <p className="text-4xl w-75 pt-4 text-white  bg-lightGreen p-8 rounded-2xl ">
-                With TAMM cleaning services! We are a team of professionals who
-                are passionate about helping you achieve your goals.
+              <p className="text-4xl w-75 pt-8 text-mainBlue bg-slate-100 p-4 rounded-2xl ">
+                TAMM cleaning services will help you experience your home like
+                never before!
+                <br />
                 <br />
                 <button className="bg-mainBlue text-white text-2xl p-3 rounded-lg mt-4 w-full hover:bg-lightBlue  ">
                   Book now!
--- tamm/src/components/ui/Header/Header.tsx
+++ tamm/src/components/ui/Header/Header.tsx
@@ -1,11 +1,12 @@
 import { Button } from "./../button";
 import Logo from "./../../../assets/FrameTAMM logo.svg";
+import Broom from "./../../../assets/broom.svg";
 
 function Header() {
   return (
-    <div className="bg-white p-4 flex justify-between text-mainBlue align-middle">
+    <div className="bg-white p-4 flex text-mainBlue align-middle">
       <img src={Logo} className="p-2" />
-      <nav className="flex p-4 w-1/2 text-right justify-end font-extrabold text-lg items-center">
+      <nav className="hidden md:flex p-4 w-1/2 text-right justify-end font-extrabold text-lg items-center">
         <a href="/" className="mr-4">
           Home
         </a>
@@ -18,10 +19,13 @@ function Header() {
         <a href="/" className="mr-4">
           Contact
         </a>
-        <Button className="p-4 bg-lightGreen text-black justify-self-center ">
+      </nav>
+      <div className="flex w-1/4 justify-end">
+        <Button className="p-4 bg-lightGreen text-black justify-self-end self-center ml-auto mx-20 md:hidden">
           Book now!
         </Button>
-      </nav>
+        <img src={Broom} className="md:hidden" />
+      </div>
     </div>
   );
 }
