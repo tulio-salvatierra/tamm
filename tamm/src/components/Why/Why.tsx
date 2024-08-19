d1ba78361c7bf11783c0c07d760770361f657ede
tuliosalvatierra <salvacorp@gmail.com>
2024-08-18 19:44:39 -0500

    chore: Update padding in App.css and font size in Why component

--- tamm/src/components/Why/Why.tsx
+++ tamm/src/components/Why/Why.tsx
@@ -8,7 +8,7 @@ function Why() {
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
-            <h1 className="text-[2rem] bg-gradient-to-b from-cyan-500 to-blue-600 bg-clip-text text-transparent font-ultrablack tracking-tighter sm:text-9xl">
+            <h1 className="text-[2rem] bg-gradient-to-b from-cyan-500 to-blue-600 bg-clip-text text-transparent font-ultrablack tracking-tighter sm:text-8xl">
               Why Choose Us?
             </h1>
             <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
