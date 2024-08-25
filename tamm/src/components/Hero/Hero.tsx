b8d8bafdb29755eea2a198463c0cd4ec6c31d527
tuliosalvatierra <salvacorp@gmail.com>
2024-08-25 13:26:16 -0500

    chore: Update CSS and SVG dimensions for improved design in About and Hero components

--- tamm/src/components/Hero/Hero.tsx
+++ tamm/src/components/Hero/Hero.tsx
@@ -7,12 +7,10 @@ function Hero() {
       <div className="container-xxl bg-inherit m-4">
         <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-1 gap-4">
           <div className="place-content-center text-left justify-evenly p- md:p-9">
-            <h1 className="text-[6rem] md:text-[10rem] font-bold mb-6 bg-gradient-to-l from-gray-200 to-green-300 bg-clip-text text-transparent tracking-tighter leading-none md:leading-[8rem]">
-              Keep your home sparkling clean{" "}
-            </h1>
-            <p className="text-4xl w-75 pt-8 text-slate-400 p-4 rounded-2xl ">
+            <h1 className="headline">Keep your home sparkling clean </h1>
+            <p className="text-4xl w-100 pt-8 text-slate-400 p-4 rounded-2xl ">
               TAMM cleaning services will have you experience your home like
-              never before!
+              <a className="text-mainBlue">never before!</a>
               <br />
               <br />
               <Button className=" text-mainBlue font-extrabold text-2xl p-6 rounded-lg mt-4 w-full hover:bg-lightBlue  ">
