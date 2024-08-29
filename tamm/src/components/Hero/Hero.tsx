e5589abd5d217f246f1541f5272d67f7c39b9ebd
tuliosalvatierra <salvacorp@gmail.com>
2024-08-29 15:34:11 -0500

    feat: Update Hero component background image and layout

--- tamm/src/components/Hero/Hero.tsx
+++ tamm/src/components/Hero/Hero.tsx
@@ -1,9 +1,17 @@
-import yellow from "./../../assets/tamm-hero.png";
+import yellow from "./../../assets/clean-home.png";
 import { Button } from "./../ui/button";
+import HeroSvg from "./../../assets/blob-scene-haikei.svg";
 
 function Hero() {
   return (
-    <section className="bg-white" id="home">
+    <section
+      id="home"
+      style={{
+        backgroundImage: `url(${HeroSvg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
       <div className="container-xxl bg-inherit py-12 md:py-24 lg:py-32">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-12 items-center">
           {/* Left Content */}
@@ -27,7 +35,7 @@ function Hero() {
             <img
               src={yellow}
               alt="cleaning"
-              className="w-full max-w-[500px] md:max-w-none md:w-80% h-auto  object-cover"
+              className="w-full max-w-[500px] md:max-w-none md:w-80% h-auto  object-cover rounded-2xl p-2 shadow-2xl"
             />
           </div>
         </div>
