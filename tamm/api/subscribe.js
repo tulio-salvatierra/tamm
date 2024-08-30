307c08701093c9ff7d63bf5450491dc4d8e91be6
Tulio Salvatierra <salvacorp@gmail.com>
2024-08-30 08:39:17 -0500

    refactor: Update subscribe.js to use ES Modules syntax for MongoDB import

--- tamm/api/subscribe.js
+++ tamm/api/subscribe.js
@@ -1,4 +1,8 @@
-const { MongoClient } = require("mongodb");
+// Change from this (CommonJS syntax):
+// const { MongoClient } = require('mongodb');
+
+// To this (ES Modules syntax):
+import { MongoClient } from "mongodb";
 
 let cachedClient = null;
 
@@ -15,33 +19,32 @@ async function connectToDatabase(uri) {
   return client;
 }
 
-module.exports = async function handler(req, res) {
-  if (req.method === "POST") {
-    const { email } = req.body;
-
-    if (!email || !/\S+@\S+\.\S+/.test(email)) {
-      return res.status(400).json({ message: "Invalid email" });
-    }
-
-    try {
-      const client = await connectToDatabase(process.env.MONGODB_URI);
-      const db = client.db("emailList");
-      const collection = db.collection("subscribedEmails");
-
-      await collection.insertOne({
-        email: email,
-        date: new Date(),
-      });
-
-      return res.status(200).json({ message: "Email saved successfully" });
-    } catch (error) {
-      console.error("Error saving email:", error);
-      return res.status(500).json({ message: "Internal server error" });
-    }
-  } else {
-    res.setHeader("Allow", ["POST"]);
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
     return res
       .status(405)
       .json({ message: `Method ${req.method} not allowed` });
   }
-};
+
+  const { email } = req.body;
+
+  if (!email || !/\S+@\S+\.\S+/.test(email)) {
+    return res.status(400).json({ message: "Invalid email" });
+  }
+
+  try {
+    const client = await connectToDatabase(process.env.MONGODB_URI);
+    const db = client.db("emailList");
+    const collection = db.collection("subscribedEmails");
+
+    await collection.insertOne({
+      email,
+      date: new Date(),
+    });
+
+    return res.status(200).json({ message: "Email saved successfully" });
+  } catch (error) {
+    console.error("Error in serverless function:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
