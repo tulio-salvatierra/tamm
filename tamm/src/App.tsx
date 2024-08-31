91b2ba136b94a3fb66122c98787b2fa1e6b18fd3
Tulio Salvatierra <salvacorp@gmail.com>
2024-08-31 11:19:51 -0500

    deleted uused code

--- tamm/src/App.tsx
+++ tamm/src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import About from "./components/About";
-import CTA from "./components/CTA";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
