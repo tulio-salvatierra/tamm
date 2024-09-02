422c68eeb00b210756ae36a4d91229aa89282673
tuliosalvatierra <salvacorp@gmail.com>
2024-09-02 09:06:37 -0500

    Corrected services route

--- tamm/src/main.tsx
+++ tamm/src/main.tsx
@@ -3,18 +3,21 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root.tsx";
 import ErrorPage from "./pages/ErrorPage.tsx";
-import "./index.css";
 import ServicesPage from "./pages/ServicesPage.tsx";
+import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "services",
-    element: <ServicesPage />,
+    errorElement: <ErrorPage />, // Handles errors in Root route
+    children: [
+      {
+        path: "services",
+        element: <ServicesPage />,
+        errorElement: <ErrorPage />, // Handles errors in Services route
+      },
+    ],
   },
 ]);
 
