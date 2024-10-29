48412c1911cef5f9d0d6fe2c4b28d8d8860c4adc
Tulio Salvatierra <salvacorp@gmail.com>
2024-10-28 20:36:48 -0500

    added scroll animation to floating button

--- tamm/src/components/floatingCallButton/FloatingCallButton.tsx
+++ tamm/src/components/floatingCallButton/FloatingCallButton.tsx
@@ -1,12 +1,49 @@
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Phone from "../../assets/phone.svg";
+
+gsap.registerPlugin(ScrollTrigger);
+
 export default function FloatingCallButton() {
+  const buttonRef = useRef(null);
+
+  useEffect(() => {
+    const config = { strength: 1 };
+
+    // Set initial position and xPercent
+    gsap.set(buttonRef.current, { xPercent: -50, x: -1 });
+
+    // Tween animation
+    gsap.to(buttonRef.current, {
+      repeat: -1,
+      yoyo: true,
+      x: 1,
+      duration: 0.2,
+      ease: "power1.inOut",
+      modifiers: {
+        x: gsap.utils.unitize((value) => value * config.strength, "px"),
+      },
+    });
+
+    // ScrollTrigger animation for config.strength
+    gsap.to(config, {
+      strength: 10,
+      ease: "none",
+      scrollTrigger: {
+        scrub: true,
+      },
+    });
+  }, []);
+
   return (
-    <div className="fixed bottom-4 right-4">
+    <div className="fixed bottom-4 right-4" ref={buttonRef}>
       <a
         href="tel:+17739199161"
-        className=" flex p-4 bg-mainBlue text-lightGreen text-lg  mb-4    rounded-full shadow-2xl"
+        aria-label="Call us now at +1 (773) 919-9161"
+        className="flex items-center p-4 bg-mainBlue text-lightGreen text-lg rounded-full shadow-2xl transition-transform transform hover:scale-105"
       >
-        <img src={Phone} className="mr-1" />
+        <img src={Phone} alt="Phone icon" className="mr-2 w-5 h-5" />
         Call now
       </a>
     </div>
