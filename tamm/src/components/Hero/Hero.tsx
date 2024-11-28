677b67ac01035eff7b24a034a7bee510f15759ec
Tulio Salvatierra <tuliosalvatierra@Tulios-Mac-Studio.local>
2024-11-28 12:22:33 -0600

    fixed hero items

--- tamm/src/components/Hero/Hero.tsx
+++ tamm/src/components/Hero/Hero.tsx
@@ -16,18 +16,19 @@ function Hero() {
         backgroundImage: `url(${HeroSvg})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
+        zIndex: -100,
       }}
     >
       <div className="container-xxl bg-inherit py-12 md:py-24 lg:py-32">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-12 items-center">
           {/* Left Content */}
           <div className="space-y-6 px-4 md:px-6 text-left z-10">
-            <h1 className="text-[5rem] leading-none bg-gradient-to-b from-cyan-500 to-blue-600 bg-clip-text text-transparent font-ultrablack tracking-tighter sm:text-8xl p-4">
+            <h1 className="text-[3rem] leading-none bg-gradient-to-b from-cyan-500 to-blue-600 bg-clip-text text-transparent font-ultrablack tracking-tighter sm:text-8xl p-4">
               Keep your home sparkling clean
             </h1>
-            <p className="p-4 bg-transparent text-4xl text-slate-400 max-w-[900px] md:text-2xl lg:text-2xl xl:text-2xl ">
+            <p className="p-4 bg-transparent text-4xl text-slate-400 max-w-[900px] md:text-lg lg:text-lg xl:text-lg ">
               TAMM cleaning services will have you experience your home like{" "}
-              <a className="text-mainBlue text-[2.5rem] leading-none tracking-tighter underline">
+              <a className="text-mainBlue text-[1.5rem] leading-none tracking-tighter underline">
                 never before!
               </a>
               <br />
