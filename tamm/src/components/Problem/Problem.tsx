7bd481e945e18d3a6b8f6a952d43bf5c355a4abc
Tulio Salvatierra <salvacorp@gmail.com>
2024-12-14 11:37:06 -0600

    fixed problem component styling

--- tamm/src/components/Problem/Problem.tsx
+++ tamm/src/components/Problem/Problem.tsx
@@ -4,19 +4,20 @@ import { problemData } from "@/components/Problem/problemData";
 
 export default function Problem() {
   return (
-    <section id="about" className="w-full py-40 my-30 md:py-24 lg:py-32">
+    <section id="about" className="w-full h-5/6 py-50 my-30 md:py-24 lg:py-40">
       <div className="container px-4 md:px-6">
         <div className="grid items-center gap-6 lg:grid-cols-2 lg:gap-12">
           <div className="space-y-4">
             <div className="inline-block text-h1 rounded-lg bg-muted pb-10 py-1leading-none bg-gradient-to-b from-cyan-500 to-blue-600 bg-clip-text text-transparent font-ultrablack tracking-tighter">
               Let’s face it:
             </div>
-            <h2 className="text-body text-mainBlue font-ultrablack sm:text-4xl">
+            <h2 className="text-sm text-mainBlue font-ultrablack sm:text-4xl bg-muted p-4 rounded-xl leading-tight line">
               trying to maintain a tidy, comfortable home is harder than it
-              should be. Instead of unwinding in a clean, welcoming space,
-              you’re stuck facing clutter, dust, and an endless chore list.
+              should be.
+              <br /> Instead of unwinding in a clean, welcoming space, you’re
+              stuck facing clutter, dust, and an endless chore list.
             </h2>
-            <div className="grid grid-cols-2 gap-4 text-left">
+            <div className="grid grid-cols-2 gap-4 text-left mt-10">
               {problemData.map((problem, index) => (
                 <div
                   className="space-y-2 bg-muted p-2 border-x-rose-950 rounded-lg"
